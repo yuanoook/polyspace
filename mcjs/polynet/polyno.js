ca4450194adcd9335f3ba12a81bf0e9488e93ec4
Rango_Yuan <Rango_Yuan@.epam.com>
2020-08-18 19:04:46 +0800

    update

---
+++ mcjs/polynet/polyno.js
@@ -0,0 +1,37 @@
+
+// [0, 0, 0, 0, 0, ...]
+function OriginPolyNumber () {
+
+}
+
+OriginPolyNumber.prototype = {
+
+}
+
+
+function LinkedPolySpace () {
+
+}
+
+LinkedPolySpace.prototype = {
+
+}
+
+function LinkedPolyNumber () {
+  this.polyNode = new PolyNode()
+}
+
+LinkedPolyNumber.prototype = {
+  set: function (value) {
+
+  },
+  get: function (value) {
+
+  },
+  setIndex: function (index, value) {
+
+  },
+  getIndex: function (index) {
+
+  }
+}
---
+++ mcjs/polynet/readme.md
@@ -0,0 +1,18 @@
+Poly numbers network
+
+#1 Origin Poly Number
+
+Dimension     Instance
+
+0             []
+1             [0] 
+2             [0, 0] 
+3             [0, 0, 0] 
+4             [0, 0, 0, 0] 
+5             [0, 0, 0, 0, 0]
+...
+n             [0(1), 0(2), 0(3), 0(4), 0(5), ...0(n)] 
+
+
+
+

cc61884d4d8b23b02105b0d51cccca7334726234
Rango Yuan <yuanoook@gmail.com>
2020-08-26 02:01:45 +0000

    hahhahahahah

--- polyspace/Space.spec.js
+++ polyspace/Space.spec.js
@@ -6,6 +6,34 @@ const {
 } = require('./utils')
 
 it('[PolySpace] [Space] [Basics]', async () => {
+
+// Complex 2nd exponential example provided by Zoe
+//   await examPolyNumbers({
+//     inputs: [1, 4, 3],
+//     expectations: [4, 1, 1],
+//     timeBudget: 50
+//   })
+
+// Interesting failure case - Example provided by Delon
+//   await examPolyNumbers({
+//     inputs: [2, 2],
+//     expectations: [2, 3]
+//   })
+
+// Simple 2nd exponential example provided by Delon
+//   await examPolyNumbers({
+//     inputs: [0, 2, 4],
+//     expectations: [1, 3, 4],
+//     timeBudget: 10
+//   })
+
+// Multiple points failure example
+//   await examPolyNumbers({
+//     inputs: [0, 1, 2, 3, 4, 50],
+//     expectations: [1, -2, 3, -4, 2, -2],
+//     timeBudget: 20
+//   })
+
   await examPolyNumbers({
     inputs: [3, 4],
     expectations: [4, 3],
@@ -19,11 +47,6 @@ it('[PolySpace] [Space] [Basics]', async () => {
   await examPolyNumbers({
     inputs: [0, 1, 2],
     expectations: [0, 1, 4].map((e, i) => e + i + 4)
-  })
-  await examPolyNumbers({
-    inputs: [0, 1, 2, 3, 4, 50],
-    expectations: [1, -2, 3, -4, 2, -2],
-    timeBudget: 20
   })
 })
 

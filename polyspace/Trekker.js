13b8d0cb21feb2c7a4261bc5925f7fae3573cdd0
Rango Yuan <yuanoook@foxmail.com>
2020-09-01 22:35:03 +0800

    all allTrekkingError error :D

--- polyspace/Trekker.js
+++ polyspace/Trekker.js
@@ -2,18 +2,55 @@ function sum(...args) {
   return [].concat(...args).reduce((s, n) => s + n, 0)
 }
 
-function makeF(log, radius) {
+function generateSmoothLog(log, radius = 0) {
   return index => {
     const start = Math.max(index - radius, 0)
-    const end = Math.min(index + radius + 1, log.length)
+    const end = Math.max(Math.min(index + radius + 1, log.length), 1)
     const samples = log.slice(start, end)
     const samplesSum = sum(samples)
     return samplesSum / samples.length
   }
 }
 
-function trekking(t, m, F) {
-  return (t+1) * F(n) - t * F(n-m)
+// Numb Trek function
+
+// F(n) = Sum(log(n-r)...log(n+r)) / (2r+1)
+
+// F(n+m) = F(n) + [F(n) - F(n-m)]
+//        = 2 * F(n) - F(n-m)
+
+// F(n+tm) = F(n) + t[F(n) - F(n-m)]
+//           (t+1) * F(n) - t * F(n-m)
+
+// What are the best numbers for - m, t, r ?
+
+function generateTrekking(log, smoothRadius, predictBaseStep = 1, predictTimes = 1) {
+  return index => {
+    const smoothLog = generateSmoothLog(log, smoothRadius)
+    const predictIndex = index + predictBaseStep * predictTimes
+    const prediction = (predictTimes + 1) * smoothLog(index) - predictTimes * smoothLog(index - predictBaseStep)
+    return [predictIndex, prediction]
+  }
+}
+
+function trekkingTranslation(nomials) {
+  return log => totalTrekkingError(nomials, log)
+}
+
+function totalTrekkingError(nomials, log) {
+  return sum(allTrekkingError(nomials, log))
+}
+
+function allTrekkingError(nomials, log) {
+  return log.map((...args) => trekkingError(nomials, ...args))
+}
+
+function trekkingError([smoothRadius, predictBaseStep, predictTimes], _, index, log) {
+  const trekking = generateTrekking(log, smoothRadius, predictBaseStep, predictTimes)
+  const [predictIndex, prediction] = trekking(index)
+  const expectationIndex = Math.min(predictIndex, log.length - 1)
+  const expectation = log[expectationIndex]
+  return (prediction - expectation) ** 2
 }
 
 function parseTrekkingLog(log) {
@@ -24,7 +61,11 @@ function parseTrekkingLog(log) {
 
 module.exports = {
   sum,
-  makeF,
-  trekking,
-  parseTrekkingLog
+  generateSmoothLog,
+  generateTrekking,
+  parseTrekkingLog,
+  allTrekkingError,
+  totalTrekkingError,
+  trekkingError,
+  trekkingTranslation
 }
--- polyspace/Trekker.spec.js
+++ polyspace/Trekker.spec.js
@@ -1,26 +1,70 @@
 const {
   sum,
-  makeF,
-  trekking,
-  parseTrekkingLog
+  generateSmoothLog,
+  generateTrekking,
+  parseTrekkingLog,
+  allTrekkingError,
+  totalTrekkingError,
+  trekkingError,
+  trekkingTranslation
 } = require('./Trekker')
 const Space = require('./Space')
 const trekkingLog = require('./data.-2+3x')
 
-it('trekking.js', async () => {
+it('Trekker.js [sum]', async () => {
   expect(sum(1,2,3)).toBe(6)
   expect(sum([1,2,3])).toBe(6)
-
-  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-  expect(makeF(log, 0)(2)).toBe(2)
-  expect(makeF(log, 2)(0)).toBe(1)
-  expect(makeF(log, 2)(1)).toBe(1.5)
-  expect(makeF(log, 2)(2)).toBe(2)
-  expect(makeF(log, 3)(3)).toBe(3)
-  expect(makeF(log, 4)(4)).toBe(4)
 })
 
-it('Trekker.spec.js', async () => {
+it('Trekker.js [generateSmoothLog]', async () => {
+  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(generateSmoothLog(log, 0)(2)).toBe(2)
+  expect(generateSmoothLog(log, 2)(0)).toBe(1)
+  expect(generateSmoothLog(log, 2)(1)).toBe(1.5)
+  expect(generateSmoothLog(log, 2)(2)).toBe(2)
+  expect(generateSmoothLog(log, 3)(3)).toBe(3)
+  expect(generateSmoothLog(log, 4)(4)).toBe(4)
+})
+
+it('Trekker.js [generateTrekking]', async () => {
+  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(generateTrekking(log, 0)(0)).toEqual([1, 0])
+  expect(generateTrekking(log, 0)(1)).toEqual([2, 2])
+  expect(generateTrekking(log, 1, 2, 2)(3)).toEqual([7, 7])
+  expect(generateTrekking(log, 1, 2, 3)(3)).toEqual([9, 9])
+})
+
+it('Trekker.js [trekkingError]', async () => {
+  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(trekkingError([0, 1, 1], null, 0, log)).toBe(1)
+
+  expect(trekkingError([0, 1, 1], null, 1, log)).toBe(0)
+  expect(trekkingError([0, 1, 1], null, 2, log)).toBe(0)
+  expect(trekkingError([0, 1, 1], null, 3, log)).toBe(0)
+
+  expect(trekkingError([1, 2, 2], null, 3, log)).toBe(0)
+  expect(trekkingError([1, 2, 3], null, 3, log)).toBe(0)
+})
+
+it('Trekker.js [allTrekkingError]', async () => {
+  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(allTrekkingError([0, 1, 1], log)).toEqual([1, 0, 0, 0, 0, 0, 0, 0, 0, 1])
+  expect(allTrekkingError([0, 1, 2], log)).toEqual([4, 0, 0, 0, 0, 0, 0, 0, 1, 4])
+  expect(allTrekkingError([0, 1, 3], log)).toEqual([9, 0, 0, 0, 0, 0, 0, 1, 4, 9])
+  expect(allTrekkingError([0, 2, 3], log)).toEqual([36, 9, 0, 0, 1, 4, 9, 16, 25, 36])
+
+  expect(allTrekkingError([1, 1, 3], log)).toEqual([1, 2.25, 0, 0, 0, 0, 0, 1, 4, 1])
+  expect(allTrekkingError([2, 1, 3], log)).toEqual([0.25, 1, 2.25, 0, 0, 0, 0, 1, 0, 0.25])
+  expect(allTrekkingError([3, 1, 1], log)).toEqual([1, 0.25, 0, 0.25, 0, 0, 0, 1, 2.25, 1])
+})
+
+it('Trekker.js [totalTrekkingError]', async () => {
+  const log = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  expect(totalTrekkingError([0, 1, 1], log)).toBe(2)
+  expect(totalTrekkingError([0, 1, 2], log)).toBe(9)
+})
+
+it('Trekker.js [parseTrekkingLog]', async () => {
   expect(1).toBe(1)
   expect(typeof trekkingLog).toBe('string')
 
@@ -28,32 +72,7 @@ it('Trekker.spec.js', async () => {
   expect(typeof log).toBe('object')
   expect(Array.isArray(log)).toBe(true)
   expect(Array.isArray(log[0])).toBe(true)
-
-  // Trekker function
-
-  // F(n+m) = F(n) + [F(n) - F(n-m)]
-  //        = 2 * F(n) - F(n-m)
-
-  // F(n+tm) = F(n) + t[F(n) - F(n-m)]
-  //           (t+1) * F(n) - t * F(n-m)
-
-  // F(n) = Sum(log(n-r)...log(n+r)) / (2r+1)
-
-  // What are the best numbers for - m, t, r ?
 })
-
-function trekkingTranslation(nomials) {
-  return log => calculateTrekkingError(nomials, log)
-}
-
-function calculateTrekkingError(nomials, log) {
-  return log.reduce((error, ...args) => error + logTrekkingError(nomials, ...args), 0)
-}
-
-function logTrekkingError(nomials, point, index, log) {
-  const length = log.length
-  const [m, t, r] = nomials
-}
 
 async function examPolyNumbers ({
   inputs,
